fd9d34c6761d194f81566720943a99fecacb135a
Casey Dwyer <caseydwyer@gmail.com>
2015-04-23 18:59:12 -0700

    Rename from conf to confmt.
    Signed-off-by: Casey Dwyer <caseydwyer@gmail.com>

---
+++ confmt/__init__.py
@@ -0,0 +1,1 @@
+from .confmt import load, loads
--- tests/test.py
+++ tests/test.py
@@ -11,11 +11,11 @@ CONFIG_FILENAME = os.path.join(TESTS_DIR, 'test.conf')
 
 sys.path.append(PARENT_DIR)
 
-import conf
+import confmt
 
 # test defaults
 with open(CONFIG_FILENAME) as f:
-    env = conf.load(f)
+    env = confmt.load(f)
     assert env['string'] == '中國 = 中国'
     assert env['int'] == -42
     assert env['float'] == 3.14
@@ -26,12 +26,12 @@ with open(CONFIG_FILENAME) as f:
 
 # test object_hook
 with open(CONFIG_FILENAME) as f:
-    env = conf.load(f, object_hook=collections.OrderedDict)
+    env = confmt.load(f, object_hook=collections.OrderedDict)
     assert isinstance(env, collections.OrderedDict)
 
 # test parse_int/float
 with open(CONFIG_FILENAME) as f:
-    env = conf.load(f, parse_int=str, parse_float=str)
+    env = confmt.load(f, parse_int=str, parse_float=str)
     assert env['string'] == '中國 = 中国'
     assert env['int'] == '-42'
     assert env['float'] == '3.14'
@@ -42,7 +42,7 @@ with open(CONFIG_FILENAME) as f:
 
 # test parse_*
 with open(CONFIG_FILENAME) as f:
-    env = conf.load(f, parse_int=float, parse_float=lambda x: int(float(x)))
+    env = confmt.load(f, parse_int=float, parse_float=lambda x: int(float(x)))
     assert isinstance(env['int'], float)
     assert env['string'] == '中國 = 中国'
     assert env['int'] == -42.0
@@ -54,7 +54,7 @@ with open(CONFIG_FILENAME) as f:
 
 # test true/false/none tokens
 with open(CONFIG_FILENAME) as f:
-    env = conf.load(f, true_token=None, false_token=None, none_token=None)
+    env = confmt.load(f, true_token=None, false_token=None, none_token=None)
     assert env['string'] == '中國 = 中国'
     assert env['int'] == -42
     assert env['float'] == 3.14
@@ -64,32 +64,32 @@ with open(CONFIG_FILENAME) as f:
     assert env[''] == ''
 
 # test true/false/none tokens
-env = conf.loads('none = none\nyes = yes\nno = no', true_token='yes',
-                 false_token='no', none_token='none')
+env = confmt.loads('none = none\nyes = yes\nno = no', true_token='yes',
+                   false_token='no', none_token='none')
 assert env['yes'] is True
 assert env['no'] is False
 assert env['none'] is None
 
 # test comment_token
-env = conf.loads('# = 1\n; = 2', comment_token=';')
+env = confmt.loads('# = 1\n; = 2', comment_token=';')
 assert env['#'] == 1
 assert ';' not in env
 
 # test assignment_operator
-env = conf.loads('one: 1\ntwo: 2\n', assignment_operator=':')
+env = confmt.loads('one: 1\ntwo: 2\n', assignment_operator=':')
 assert env['one'] == 1
 assert env['two'] == 2
 
 # test strict_mode
 true = False
 try:
-    conf.loads('error', strict_mode=True)
+    confmt.loads('error', strict_mode=True)
 except ValueError:
     true = True
 assert true
 
 # test variable_validator
-env = conf.loads('one_love = 1\ntwo doves = 2',
-                 variable_validator=lambda s: ' ' not in s)
+env = confmt.loads('one_love = 1\ntwo doves = 2',
+                   variable_validator=lambda s: ' ' not in s)
 assert env['one_love'] == 1
 assert 'two doves' not in env
