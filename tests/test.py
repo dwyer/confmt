f78f7ba5ac8e3b38deb92e416906552c77554d52
Casey Dwyer <caseydwyer@gmail.com>
2017-03-02 21:39:38 -0800

    tests/tests.py: Change conf alias to simpleconf.

--- tests/test.py
+++ tests/test.py
@@ -6,7 +6,7 @@ import os
 import re
 import sys
 
-import simpleconf as conf
+import simpleconf
 
 TESTS_DIR = os.path.dirname(os.path.abspath(__file__))
 PARENT_DIR = os.path.dirname(TESTS_DIR)
@@ -24,7 +24,7 @@ def compare(a, b):
 
 # test defaults
 with open(CONFIG_FILENAME) as f:
-    env = conf.load(f)
+    env = simpleconf.load(f)
     baseenv = env
     assert env['string'] == '中國 = 中国'
     assert env['int'] == -42
@@ -34,27 +34,28 @@ with open(CONFIG_FILENAME) as f:
     assert env['none'] is None
     assert env[''] == ''
     compare(baseenv, env)
-    content = sorted(conf.dumps(env).splitlines())
+    content = sorted(simpleconf.dumps(env).splitlines())
 
 # test object_type
 with open(CONFIG_FILENAME) as f:
-    env = conf.load(f, object_type=collections.OrderedDict)
+    env = simpleconf.load(f, object_type=collections.OrderedDict)
     assert isinstance(env, collections.OrderedDict)
     compare(baseenv, env)
-    compare(content, sorted(conf.dumps(env).splitlines()))
+    compare(content, sorted(simpleconf.dumps(env).splitlines()))
 
 # test parse_int/float
 with open(CONFIG_FILENAME) as f:
-    env = conf.load(f, parse_int=str, parse_float=str)
+    env = simpleconf.load(f, parse_int=str, parse_float=str)
     tempenv = baseenv.copy()
     tempenv['int'] = str(tempenv['int'])
     tempenv['float'] = str(tempenv['float'])
     compare(env, tempenv)
-    compare(content, sorted(conf.dumps(env).splitlines()))
+    compare(content, sorted(simpleconf.dumps(env).splitlines()))
 
 # test parse_*
 with open(CONFIG_FILENAME) as f:
-    env = conf.load(f, parse_int=float, parse_float=lambda x: int(float(x)))
+    env = simpleconf.load(f, parse_int=float,
+                          parse_float=lambda x: int(float(x)))
     assert isinstance(env['int'], float)
     assert isinstance(env['float'], int)
     tempenv = baseenv.copy()
@@ -63,12 +64,12 @@ with open(CONFIG_FILENAME) as f:
 
 # test true/false/none tokens
 with open(CONFIG_FILENAME) as f:
-    env = conf.load(f, keywords={})
+    env = simpleconf.load(f, keywords={})
     tempenv = baseenv.copy()
     tempenv['yes'] = 'true'
     tempenv['no'] = 'false'
     tempenv['none'] = 'null'
-    compare(content, sorted(conf.dumps(env).splitlines()))
+    compare(content, sorted(simpleconf.dumps(env).splitlines()))
 
 # test true/false/none tokens
 with open(CONFIG_FILENAME) as f:
@@ -76,66 +77,71 @@ with open(CONFIG_FILENAME) as f:
     s = s.replace('true', 'yes')
     s = s.replace('false', 'no')
     s = s.replace('null', 'none')
-    env = conf.loads(s, keywords={'yes': True, 'no': False, 'none': None})
+    env = simpleconf.loads(s,
+                           keywords={'yes': True,
+                                     'no': False,
+                                     'none': None})
     compare(env, baseenv)
-    compare(content, sorted(conf.dumps(env).splitlines()))
+    compare(content, sorted(simpleconf.dumps(env).splitlines()))
 
 # test comment_token
 with open(CONFIG_FILENAME) as f:
     s = f.read().replace('#', ';')
-    env = conf.loads(s, comment_token=';')
+    env = simpleconf.loads(s, comment_token=';')
     compare(env, baseenv)
-    compare(content, sorted(conf.dumps(env).splitlines()))
+    compare(content, sorted(simpleconf.dumps(env).splitlines()))
 
 # test assignment_operator
 with open(CONFIG_FILENAME) as f:
     s = '\n'.join(line.replace('=', ':', 1) for line in f)
-    env = conf.loads(s, assignment_operator=':')
+    env = simpleconf.loads(s, assignment_operator=':')
     compare(env, baseenv)
-    compare(content, sorted(conf.dumps(env).splitlines()))
+    compare(content, sorted(simpleconf.dumps(env).splitlines()))
 
 # test strict_mode
 with open(CONFIG_FILENAME) as f:
     true = False
     try:
-        conf.load(f, strict=True)
+        simpleconf.load(f, strict=True)
     except ValueError:
         true = True
     assert true
 
 # test key_validator
 with open(CONFIG_FILENAME) as f:
-    env = conf.load(f, key_validator=lambda s: re.match(r'[\w_][\w\d_]*', s))
+    env = simpleconf.load(
+        f, key_validator=lambda s: re.match(r'[\w_][\w\d_]*', s))
     tempenv = baseenv.copy()
     del tempenv['']
     compare(env, tempenv)
-    compare(content[1:], sorted(conf.dumps(env).splitlines()))
+    compare(content[1:], sorted(simpleconf.dumps(env).splitlines()))
 
     def valid(s):
         return ' ' not in s
 
-    env = conf.loads('a.b = 1 \n a.b.c d = 2', key_separator='.',
-                     key_validator=valid)
+    env = simpleconf.loads('a.b = 1 \n a.b.c d = 2', key_separator='.',
+                           key_validator=valid)
     assert env['a']['b'] == 1
-    env = conf.loads('a.b c.d = 1', key_separator='.', key_validator=valid)
+    env = simpleconf.loads('a.b c.d = 1', key_separator='.',
+                           key_validator=valid)
     assert not env
 
 # test key_separator
 with os.popen('git config --list --local') as f:
-    env = conf.load(f, key_separator='.', parse_int=float)
+    env = simpleconf.load(f, key_separator='.', parse_int=float)
     opts = dict(key_separator='->', keywords={'yes': True, 'no': False})
-    s = conf.dumps(env, **opts)
-    compare(env, conf.loads(s, **opts))
+    s = simpleconf.dumps(env, **opts)
+    compare(env, simpleconf.loads(s, **opts))
 
 # test escape_token
 s = 'a \= b = \#t # this should be #t'
 keywords = {'#t': True, '#f': False}
-obj = conf.loads(s, keywords=keywords)
+obj = simpleconf.loads(s, keywords=keywords)
 assert 'a = b' in obj and obj['a = b'] is True, obj
-t = conf.dumps({'a = b': True}, keywords=keywords)
+t = simpleconf.dumps({'a = b': True}, keywords=keywords)
 assert s.startswith(t), (s, t)
 
-obj = conf.loads('a.b = \#t \n a\.b = \#f', keywords=keywords,
-                 key_separator='.')
+obj = simpleconf.loads('a.b = \#t \n a\.b = \#f', keywords=keywords,
+                       key_separator='.')
 assert 'a' in obj and 'b' in obj['a'] and obj['a']['b'] is True, obj
 assert 'a.b' in obj and obj['a.b'] is False, obj
